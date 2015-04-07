4231ab34ef1cfe165b18b7713814189e43df9339
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-04-07 19:11:13 +0200

    initial commit

---
+++ js/termometr.js
@@ -0,0 +1,14 @@
+var form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', function(e) {
+  var celsjusz = form.celsjusz.value; // Przypisaliśmy wartość podaną przez użytkownika do zmiennej
+  e.preventDefault(); // Zapobiegamy domyślnej akcji przeglądarki, czyli przeładowania strony
+  var convert = parseFloat(celsjusz);
+ para.innerText =  celsjusToFahrenheit(convert);// Tutaj będziemy wywoływać funkcję i przypisywać wynik do elementu.
+});
+var celsjusToFahrenheit = function(cel){
+	if (typeof(cel)==="number"){
+	return (cel*2) + 30;
+	}
+}
+var para = document.createElement('p');
+document.body.appendChild(para);
